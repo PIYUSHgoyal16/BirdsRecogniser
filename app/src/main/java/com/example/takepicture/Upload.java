c62886129d23fb7ba38a1534b8b5e24c772ee193
Piyush Goyal <piyushgoyaliit@gmail.com>
2020-02-02 16:12:52 +0530

    Initial commit

---
+++ app/src/main/java/com/example/takepicture/Upload.java
@@ -0,0 +1,36 @@
+package com.example.takepicture;
+
+public class Upload {
+    private String mName;
+    private String ImageUrl;
+
+    public Upload(){
+        //Empty Constructor
+    }
+
+    public Upload(String name, String url){
+        ImageUrl = url;
+        if(name.trim().equals("")){
+            mName = "No Name";
+        }
+        else{
+            mName = name;
+        }
+    }
+
+    public String getmName() {
+        return mName;
+    }
+
+    public String getImageUrl() {
+        return ImageUrl;
+    }
+
+    public void setmName(String mName) {
+        this.mName = mName;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        ImageUrl = imageUrl;
+    }
+}
